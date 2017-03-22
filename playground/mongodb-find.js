a358f34ee282bbdc0e73a5e1f650349a42474c63
Lewis Nkwo <lewisnkwo@Lewiss-MacBook-Pro.local>
2017-03-22 02:05:44 +0000

    Add find and delete scripts

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -8,20 +8,20 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').find({
-  //   _id: new ObjectID('58d1691eaaa02713679d525e')
-  // }).toArray().then((docs) => {
-  //   console.log('Todos');
-  //   console.log(JSON.stringify(docs, undefined, 2));
-  // }, (err) => {
-  //   console.log('Unable to fetch todos.', err);
-  // })
+  db.collection('Todos').find({
+    _id: new ObjectID('58d1691eaaa02713679d525e')
+  }).toArray().then((docs) => {
+    console.log('Todos');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch todos.', err);
+  })
 
-  // db.collection('Todos').find().count().then((count) => {
-  //   console.log(`Todos Count: ${count}`);
-  // }, (err) => {
-  //   console.log('Unable to fetch todos.', err);
-  // })
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos Count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch todos.', err);
+  })
 
   db.collection('Users').find({name: 'Lewis'}).count().then((count) => {
     console.log(`Found ${count} users with the name 'Lewis'.`);
