ea0e43fbd9a4098535ff372e2af1fb513b67a219
Lewis Nkwo <lewisnkwo@Lewiss-MacBook-Pro.local>
2017-03-29 16:25:27 +0100

    Add POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,38 @@
+// const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+let data = {
+  id: 10
+};
+
+let token = jwt.sign(data, '123abc');
+console.log(token);
+
+let decoded = jwt.verify(token, '123abc');
+console.log('Decoded:', decoded);
+
+
+// let message = 'I am user number 2';
+// let hash = SHA256('message').toString();
+//
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// let data = {
+//   id: 4
+// };
+// let token = {
+//   data,
+//   hash: SHA256(JSON.stringify(data) + 'secret').toString()
+// };
+//
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+//
+// let resultHash = SHA256(JSON.stringify(token.data) + 'secret').toString();
+//
+// if (resultHash === token.data) {
+//   console.log('Data was not changed.');
+// } else {
+//   console.log('Data was changed. Do not trust!');
+// };
--- server/server.js
+++ server/server.js
@@ -28,6 +28,20 @@ app.post('/todos', (req, res) => {
 
 });
 
+app.post('/users', (req, res) => {
+
+  let user = new User(_.pick(req.body, ['email', '[password]']));
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+
+});
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos});
